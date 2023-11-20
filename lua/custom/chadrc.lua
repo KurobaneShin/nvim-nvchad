ce82e4241573a7d57c9530fd5fc19aa1eec88862
icaro <darkflamemaster663@gmail.com>
2023-11-20 12:25:42 -0300

    custom config

---
+++ lua/custom/chadrc.lua
@@ -0,0 +1,26 @@
+---@type ChadrcConfig
+local M = {}
+
+-- Path to overriding theme and highlights files
+local highlights = require "custom.highlights"
+
+M.ui = {
+  theme = "catppuccin",
+  theme_toggle = { "catppuccin", "one_light" },
+
+  hl_override = highlights.override,
+  hl_add = highlights.add,
+}
+
+for i = 1, 9, 1 do
+  vim.keymap.set("n", string.format("<A-%s>", i), function()
+    vim.api.nvim_set_current_buf(vim.t.bufs[i])
+  end)
+end
+
+M.plugins = "custom.plugins"
+
+-- check core.mappings for table structure
+M.mappings = require "custom.mappings"
+
+return M
---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,62 @@
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+
+local lspconfig = require "lspconfig"
+local configs = require "lspconfig/configs"
+-- if you just want default config for the servers then put them in a table
+local servers = {
+  "html",
+  "cssls",
+  "tsserver",
+  "clangd",
+  "eslint",
+  "tailwindcss",
+  "docker_compose_language_service",
+  "quick_lint_js",
+}
+--test htmx lsp
+lspconfig.gopls.setup {
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+      gofumpt = true,
+    },
+  },
+}
+
+if not configs.golangcilsp then
+  configs.golangcilsp = {
+    default_config = {
+      cmd = { "golangci-lint-langserver" },
+      root_dir = lspconfig.util.root_pattern(".git", "go.mod"),
+      init_options = {
+        command = {
+          "golangci-lint",
+          "run",
+          "--enable-all",
+          "--disable",
+          "lll",
+          "--out-format",
+          "json",
+          "--issues-exit-code=1",
+        },
+      },
+    },
+  }
+end
+lspconfig.golangci_lint_ls.setup {
+  filetypes = { "go", "gomod" },
+}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
+
+--
+-- lspconfig.pyright.setup { blabla}
