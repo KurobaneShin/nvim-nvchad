a6013904564f2b2194039ca68d28f677f5cdffcb
kurobane <darkflamemaster663@gmail.com>
2024-02-09 19:58:22 -0300

    add surround plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -74,6 +74,16 @@ local plugins = {
   },
 
   {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup {
+        -- Configuration here, or leave empty to use defaults
+      }
+    end,
+  },
+  {
     "ThePrimeagen/refactoring.nvim",
     lazy = false,
     dependencies = {
